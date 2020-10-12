d6285216413efed1e4c8f110e58568b266f9bff5
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-10-12 16:10:31 -0300

    Started design of dashboard

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 
+import Header from '../../components/Header';
+
 export default function Dashboard() {
 
   return (
-    <div className="div"></div>
+    <Header />
   );
 }
